d8b71275a0bfb11ce918be158826e7f99ec518c0
nitesh <nitesh26028@gmail.com>
2024-04-17 15:32:13 +0530

    erhn

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from flask import  Flask, render_template, request, redirect
+from flask import Flask
+from flask import  Flask, render_template, request, redirect,flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail, Message
-
+import os
 
 app = Flask(__name__)
+
+app.secret_key ="cchghhvhvhjhvvhvhvh"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///myfirstdb.db"
 
@@ -18,13 +21,13 @@ app.config['MAIL_PASSWORD'] = 'etzm cfxs yeex lawp'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
-
+2
 
 class ContactUs(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Title = db.Column(db.String(120))
     Message = db.Column(db.Text)
-
+    MYimage = db.Column(db.String(255))
 # db.create_all()
     
 with app.app_context():
@@ -50,25 +53,31 @@ def servicespage():
 
     return render_template("services.html", mydata = data)
 
-@app.route("/savethisdata", methods = ["post"])
+@app.route("/savethisdata", methods = ["POST"])
 def savethisdata(): 
     if request.method == "POST":
         mytitle = request.form.get("title")
-        message = request.form.get("msg")
+        mymessage = request.form.get("msg")
+        myimg=request.files.get("img")
+        if myimg:
+            myimg.save(os.path.join("static/images", myimg.filename))
+            path = os.path.join("static/images", myimg.filename)
 
-        data = ContactUs(Title = mytitle, Message = message)
+        data = ContactUs(Title = mytitle, Message = mymessage, MYimage = path)
+        # data = ContactUs(Title = mytitle, Message = message)
         db.session.add(data)
         db.session.commit()
+        flash("Data Sucessfully saved in database..")
+        flash("Check data saved in database")
+        flash("Done")
 
-
-
-        msg = Message('Hello', sender = 'nitesh26028@gmail.com', recipients = ['nitesh26028@gmail.com'])
-        msg.body = "Hello Flask message sent from Flask-Mail"
-        mail.send(msg)
+        # msg = Message('Hello', sender = 'nitesh26028@gmail.com', recipients = ['nitesh26028@gmail.com'])
+        # msg.body = "Hello Flask message sent from Flask-Mail"
+        # mail.send(msg)
         # return "Setnt"
         return redirect("/contact")
 
-    return "your data saved sucessfulyyy......!"
+    # return "your data saved sucessfulyyy......!"
 
 
 @app.route("/deletethisdata/<int:x>", methods = ["POST"])
